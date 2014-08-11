3fc3557199aaa036fd47bf2b1b04be65e430bc7e
Joseph Wu <kaysoky88@gmail.com>
2014-08-11 13:47:43 -0700

    Improve example batch insert

--- examples/foobar.c
+++ examples/foobar.c
@@ -3,6 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static void GenerateID(char generatedId[25]) {
+ bson_context_t* context = bson_context_new(
+ (bson_context_flags_t) (BSON_CONTEXT_THREAD_SAFE | BSON_CONTEXT_DISABLE_PID_CACHE));
+
+ bson_oid_t oid;
+ bson_oid_init(&oid, context);
+ bson_oid_to_string(&oid, generatedId);
+}
+
 static void EnsureIndex(mongoc_collection_t* collection, const char* name, bson_t* indexDef, bool unique) {
   bson_error_t error;
   mongoc_index_opt_t opts;
@@ -41,18 +50,22 @@ static void DoStuff(mongoc_client_t* client, const uint32_t payload_length) {
   EnsureIndex(collection, "c", indexDef, false);
   
   // Start timing
-  struct timeval startTime, currentTime;
+  struct timeval startTime, beforeExecTime, currentTime;
   gettimeofday(&startTime, NULL);
   uint64_t startusec = startTime.tv_sec * 1000000 + startTime.tv_usec;
   printf("Num Inserts,Usec Elapsed\n");
   
   // Build and insert the documents
+  uint64_t totalExecuteTime = 0;
   mongoc_bulk_operation_t* bulk = NULL;
   for (uint32_t i = 0; i < payload_length; i++) {
     if (i % 300 == 0) {
       
       // Execute!
       if (bulk != NULL) {
+          gettimeofday(&beforeExecTime, NULL);
+          uint64_t beforeExecute = beforeExecTime.tv_sec * 1000000 + beforeExecTime.tv_usec;
+          
           bson_t* reply = bson_new();
           mongoc_bulk_operation_execute(bulk, reply, &error);
           
@@ -62,17 +75,26 @@ static void DoStuff(mongoc_client_t* client, const uint32_t payload_length) {
           gettimeofday(&currentTime, NULL);
           uint64_t currentusec = currentTime.tv_sec * 1000000 + currentTime.tv_usec;
           printf("%d,%llu\n", i, currentusec - startusec);
+          
+          totalExecuteTime += currentusec - beforeExecute;
       }
       
       bulk = mongoc_collection_create_bulk_operation(collection, true, NULL);
     }
   
+    char generatedId[25];
+    GenerateID(generatedId);
+    
     bson_t* document = bson_new();
     bson_append_int32(document, "a", 1, i);
     bson_append_int32(document, "c", 1, i * 100);
+    bson_append_utf8(document, "_key", 4, generatedId, 25);
+    bson_append_utf8(document, "_user", 5, "nobody", 6);
     mongoc_bulk_operation_insert(bulk, document);
     bson_destroy(document);
   }
+  
+  printf("Total Execution Time: %llu usec\n", totalExecuteTime);
 }
 
 int main (int argc, char *argv[]) {
